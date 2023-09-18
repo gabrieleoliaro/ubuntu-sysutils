e580217b4f410020d99ea952e6b6b8eb5946f82b
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-09-18 01:34:03 +0000

    update

--- github-runner/install_runner.sh
+++ github-runner/install_runner.sh
@@ -8,11 +8,11 @@ cd "${BASH_SOURCE[0]%/*}"
 # Create a folder
 mkdir actions-runner && cd actions-runner
 # Download the latest runner package
-curl -o actions-runner-linux-x64-2.299.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-2.299.1.tar.gz
+curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz
 # Optional: Validate the hash
-echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  actions-runner-linux-x64-2.299.1.tar.gz" | shasum -a 256 -c
+echo "2974243bab2a282349ac833475d241d5273605d3628f0685bd07fb5530f9bb1a  actions-runner-linux-x64-2.309.0.tar.gz" | shasum -a 256 -c
 # Extract the installer
-tar xzf ./actions-runner-linux-x64-2.299.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.309.0.tar.gz
 # Install dependencies
 sudo apt-get upgrade -y && sudo apt-get update -y
 sudo ./bin/installdependencies.sh
